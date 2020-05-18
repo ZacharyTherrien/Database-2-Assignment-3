2ddbb8134eae0a3426c667eb32a8952a282907ec
Zachary Therrien <ztherrien.work@gmail.com>
2020-05-18 17:42:40 -0400

    Added style file and home page additions
    Just some home page additions and a blank style page has been created.

--- index.php
+++ index.php
@@ -2,9 +2,29 @@
 <body>
     <h1>Amiibo Store!</h1>
     <h2>Home Page</h2>
+	<fieldset>
+    <b><p>Store Description.</p></b>
+    <p>As the name implies, we are the ‘Amiibo Store’. Our establishment specializes in selling amiibo figures. Our target demographic consists mostly of young children and young adults who enjoy gaming on compatible Nintendo consoles.</p>
+    </fieldset>
+	<br>
+	<fieldset>
+		<b><p>Store Description</p></b>
+		<p>As the name implies, we are the ‘Amiibo Store’. Our establishment specializes in selling amiibo figures. Our target demographic consists mostly of young children and young adults who enjoy gaming on compatible Nintendo consoles.</p>
+	</fieldset>
+	<br>
+	<fieldset>
+		<b><p>Hottest Products</p></b>
+		<p>Here are our the bestselling, literally flying off the shelves. Make sure to lock your purchase before it's too late!</p>
+	</fieldset>
+	<br>
+	<footer>
+		<h2>Footer</h2>
+	</footer>
+	<!-- 
+        What should be displayed on the home page.
+
+    -->
     <?php
-        echo 3425435453254;
-        echo 'hi';
-        echo 'hi';
+	
     ?>
 </body>
