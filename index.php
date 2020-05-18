aa88000126eedab3247140970fc68aebccc7c977
Zachary Therrien <ztherrien.work@gmail.com>
2020-05-18 19:15:00 -0400

    Today's Progress
    Add a main page with some text in HTML and some CSS.

--- index.php
+++ index.php
@@ -1,25 +1,60 @@
 <!DOCTYPE html>
-<body>
-    <h1>Amiibo Store!</h1>
-    <h2>Home Page</h2>
-	<fieldset>
-    <b><p>Store Description.</p></b>
-    <p>As the name implies, we are the ‘Amiibo Store’. Our establishment specializes in selling amiibo figures. Our target demographic consists mostly of young children and young adults who enjoy gaming on compatible Nintendo consoles.</p>
-    </fieldset>
-	<br>
-	<fieldset>
-		<b><p>Store Description</p></b>
-		<p>As the name implies, we are the ‘Amiibo Store’. Our establishment specializes in selling amiibo figures. Our target demographic consists mostly of young children and young adults who enjoy gaming on compatible Nintendo consoles.</p>
-	</fieldset>
-	<br>
-	<fieldset>
-		<b><p>Hottest Products</p></b>
-		<p>Here are our the bestselling, literally flying off the shelves. Make sure to lock your purchase before it's too late!</p>
-	</fieldset>
-	<br>
-	<footer>
-		<h2>Footer</h2>
-	</footer>
+<!--
+    Questions to ask Vik:
+    - How to store BLOBs?
+    - Do passwords need to be hashed and then stores?
+    - Will the collection trigger work if making a purchase?
+
+
+    Ideas:
+    - A collection home page: demonstrate a bunch of collections.
+    - A page for individual collections.
+-->
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+    <title>Amiibo Store</title>
+</head>
+<container>
+    <body>
+        <header>
+            <h1>Amiibo Store!</h1>
+        </header>
+        <nav>
+            <ul id="navigationBar">
+                <li class="naviationItem"><a href="index.php">Home Page</a></li>
+                <li class="naviationItem"><a href="news.asp">News</a></li>
+                <li class="naviationItem"><a href="contact.asp">Contact</a></li>
+                <li class="naviationItem"><a href="about.asp">About</a></li>
+            </ul>
+        </nav>
+        <main>
+            <fieldset>
+            <b><p>Store Description.</p></b>
+            <p>As the name implies, we are the ‘Amiibo Store’. Our establishment specializes 
+            in selling amiibo figures. Our target demographic consists mostly of young children and 
+            young adults who enjoy gaming on compatible Nintendo consoles.</p>
+            </fieldset>
+            <br>
+            <fieldset>
+                <b><p>Store Description</p></b>
+                <p>As the name implies, we are the ‘Amiibo Store’. Our establishment specializes 
+                in selling amiibo figures. Our target demographic consists mostly of young children 
+                and young adults who enjoy gaming on compatible Nintendo consoles.</p>
+            </fieldset>
+            <br>
+            <fieldset>
+                <b><p>Hottest Products</p></b>
+                <p>Here are our the bestselling, literally flying off the shelves. 
+                Make sure to lock your purchase before it's too late!</p>
+            </fieldset>
+            <br>
+        </main>
+        <footer>
+            <h2>Footer</h2>
+        </footer>
+    </body>
+</container>
 	<!-- 
         What should be displayed on the home page.
 
@@ -27,4 +62,3 @@
     <?php
 	
     ?>
-</body>
